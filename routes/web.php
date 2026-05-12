28db3c3f5f3f55fb59d09e88048afd4a82846462
Tech With Ulunji <techwithulunji@gmail.com>
2026-05-12 17:13:04 +0200

    Defined add developer route

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,5 @@ use App\Http\Controllers\DeveloperController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [DeveloperController::class, 'showAll']);
+
+Route::get('/add', [DeveloperController::class, 'addNew']);
