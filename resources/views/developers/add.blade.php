624a6d5fc883453136039978962f68576ba19ffa
Tech With Ulunji <techwithulunji@gmail.com>
2026-05-21 13:32:14 +0200

    Added error messages to the form

--- resources/views/developers/add.blade.php
+++ resources/views/developers/add.blade.php
@@ -17,47 +17,68 @@
     </div>
         <h2 class="text-2xl text-center">Add Developer</h2>
     
-    <form action="{{ route('developers.store') }}" method="POST" class="bg-white mx-auto my-5 px-5 py-2.5 rounded-sm w-2/3">
+    <form action="{{ route('developers.store') }}" method="POST" class="bg-white mx-auto my-5 px-5 py-2.5 rounded-sm w-[60%]">
         @csrf
 
         {{-- name --}}
         <div>
-            <label for="name">Name:</label>
-            <input type="text" name="name" id="name" required />
+            <div>
+                <label for="name">Name</label>
+            </div>
+            <input type="text" name="name" id="name" required value="{{ old('name') }}"  class="px-2.5 py-0.5 border border-gray-300 rounded-sm outline-0 w-full" />
         </div>
 
         {{-- role --}}
         <div>
-            <label for="role">Role:</label>
-            <input type="text" name="role" id="role" />
+            <div>
+                <label for="role">Role</label>
+            </div>
+            <input type="text" name="role" id="role" value="{{ old('role') }}"  class="px-2.5 py-0.5 border border-gray-300 rounded-sm outline-0 w-full" />
         </div>
 
         {{-- email --}}
         <div>
-            <label for="email">Email:</label>
-            <input type="email" name="email" id="email">
+            <div>
+                <label for="email">Email</label>
+            </div>
+            <input type="email" name="email" id="email" value="{{ old('email') }}" class="px-2.5 py-0.5 border border-gray-300 rounded-sm outline-0 w-full">
         </div>
 
         {{-- experience --}}
         <div>
-            <label for="experience">Experience</label>
-            <input type="number" name="experience" id="experience">
+            <div>
+                <label for="experience">Experience</label>
+            </div>
+            <input type="number" name="experience" id="experience" value="{{ old('experience') }}" class="px-2.5 py-0.5 border border-gray-300 rounded-sm outline-0 w-full">
         </div>
 
         {{-- description --}}
         <div>
-            <label for="description">Description</label>
-            <textarea name="description" id="description"></textarea>
+            <div>
+                <label for="description">Description</label>
+            </div>
+            <textarea name="description" id="description" class="px-2.5 py-0.5 border border-gray-300 rounded-sm outline-0 w-full"></textarea>
         </div>
 
         {{-- company_id --}}
-        <select name="company_id" id="company_id">
+        <select name="company_id" id="company_id" class="bg-gray-200 py-0.5 w-full">
             <option value="" selected disabled>Select Company</option>
             @foreach ($companies as $company)
                 <option value="{{ $company->id }}">{{$company->name}}</option>
             @endforeach
         </select>
 
-        <input type="submit" value="Add Developer">
+        <input type="submit" value="Add Developer" class="bg-gray-200 my-2.5 px-5 py-1.5 rounded cursor-pointer">
+
+        {{-- Output Errors --}}
+        <div>
+            @if($errors->any())
+                <ul class="bg-red-100 px-2.5 py-1.5">
+                    @foreach ($errors->all() as $error)
+                        <li class="text-red-500 text-sm">{{$error}}</li>
+                    @endforeach
+                </ul>
+            @endif
+        </div>
     </form>
 </x-layout>
