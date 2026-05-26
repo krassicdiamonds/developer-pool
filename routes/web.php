b1154d44e18c29cc7caf7ca6b7150af6a4768edf
Tech With Ulunji <techwithulunji@gmail.com>
2026-05-26 14:16:15 +0200

    Added auth controller to handler authentication logic

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,9 @@
 use App\Http\Controllers\DeveloperController;
 use Illuminate\Support\Facades\Route;
 
+// Belongs to developer model
 Route::get('/', [DeveloperController::class, 'showAll'])->name('developers.index');
-
 Route::get('/add', [DeveloperController::class, 'addNew'])->name('developers.add');
-
 Route::get('/show/{id}', [DeveloperController::class, 'showDetails'])->name('developers.show');
-
-
 Route::post('/', [DeveloperController::class, 'store'])->name('developers.store');
-
 Route::delete('/destroy/{id}', [DeveloperController::class, 'destroy'])->name('developers.destroy');
